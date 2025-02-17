6c58b787505d217d4ee1a5f556777cfbe2e166eb
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2025-02-17 12:10:28 +0530

    Update index.js

--- front/src/index.js
+++ front/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+const AudioPlayer = () => (
+  <audio autoPlay loop>
+    <source src="/bg.mp3" type="audio/mp3" />
+    Your browser does not support the audio element.
+  </audio>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AudioPlayer />
     <App/>
   </React.StrictMode>
 );
